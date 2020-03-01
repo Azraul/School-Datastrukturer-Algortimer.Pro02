c5472b21a0e43b4c34f166ed79dbb41aafc3ec97
Azraul <dahkaros@gmail.com>
2020-03-01 23:53:02 +0200

    downloaded .txt files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/******************************************************
+ * ***  By Kristoffer Kuvaja                        ***
+ * ***  Arcada 2020                                 ***
+ * ***  Projekt 1, Datastrukturer och Algoritmer    ***
+ *****************************************************/
+
+
+public class Main {
+    public static void main(String[] args) {
+        new Analyzer();
+    }
+}
