ed6bfd42bb8a9d02bb7e04f6ed4c2fec040e0b43
Azraul <dahkaros@gmail.com>
2020-03-19 01:05:07 +0200

    started fileinput script

---
+++ src/Analyzer_App/FileInput.java
@@ -0,0 +1,38 @@
+package Analyzer_App;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInput {
+    public static String readFile(String URL){
+        String text = new String();
+
+        try{
+            //Öppna ne fil med en specifik URL
+            FileInputStream f = new FileInputStream(URL);
+
+            //hit spara vi ASCII-koden för ett enskilt tecken
+            //vi läser ur filen
+            int tempChar;
+
+            //Här lämnar vi in texten från filen TECKEN FÖR TECKEN
+            //f.read() returnerar ASCII-koden för det tecken
+            //den läser från frilen. f.read() returner -1
+            while ( (tempChar = f.read() ) !=-1){
+                //Till strängen text sätter vi tell det nya tecknet
+                //vi nuss läste bort från textfilen
+                text += String.valueOf((char)tempChar);
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("File was not found: "+ ex.getMessage());
+        }
+        catch (IOException ex){
+            System.out.println("IOException occured: "+ ex.getMessage());
+        }
+        //returnerar den text vi läste in från filen
+
+        return text;
+    }
+}
